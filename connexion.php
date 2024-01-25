870bab6d3a015a002d1574669a43131f9edebe68
Bananabenj <benj.thibout@gmail.com>
2024-01-25 17:54:26 +0100

    refactoring verifieToken() dans classe connexion

--- connexion.php
+++ connexion.php
@@ -30,6 +30,7 @@ class Connexion{
         $_SESSION['tokenCreation'] = time();
 
         return $string;
+    }
+
+    protected function verifieToken(){return (time()- $_SESSION['tokenCreation']  <= 180 && isset($_POST['token']) && $_POST['token']==$_SESSION['token'] ); }
 }
-}
-?>
